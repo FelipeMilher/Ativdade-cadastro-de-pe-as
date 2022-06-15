2661119b4cbe19e0938cc1819cd2c13a78c9645d
Felipe Milher <zfelipem5@gmail.com>
2022-06-15 11:59:02 -0300

    amertobino

---
+++ peças atividade.js
@@ -0,0 +1,21 @@
+let pesoPecas = 100
+let numeroPecas = 17
+let nomePeca = "Pa"
+
+if(pesoPecas >= 100) {
+    console.log("Pode cadastrar a peca")
+} else {
+    console.log("Peso insuficiente")
+}
+
+if (numeroPecas >=10) {
+    console.log("impossivel cadastrar, numero pecas excedido")
+} else {
+    console.log("Cadastro permitido")
+}
+
+if (nomePeca.length < 3) {
+    console.log ("Numero de caracateres insuficiente")
+} else {
+    console.log("Nome adequado de acordo com as diretivas")
+}
